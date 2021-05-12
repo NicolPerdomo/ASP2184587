ce328e5a85a2510c9e4de1c418dd28ba63c015b8
nicol <dayahiguera@hotmail.com>
2021-05-11 23:46:54 -0500

    first commit

---
+++ ASP 2184587/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//IMPORTANDO LOS MODELOS DE LA BASE DE DATOS 
+using ASP_2184587.Models;
+
+namespace ASP_2184587.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using(var db= new inventarioEntities())
+            {
+                return View(db.usuario.ToList());
+
+            }
+           
+        }
+    }
+}
